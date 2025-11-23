ac52f60b68344a4cde69626b8d3ec6167c2ef306
Stephen Suffian <stephen.suffian@pm.me>
2025-11-22 22:01:31 -0500

    Update package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	output: 'server',
+	adapter: vercel(),
 	integrations: [mdx(), sitemap()],
 });
